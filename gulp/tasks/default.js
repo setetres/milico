8851151c83a85109dfe1c10ee7559a437c125b53
Guilherme Pangnotta <setetres@gmail.com>
2021-07-10 13:32:30 -0300

    wip

--- gulp/tasks/default.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var runSequence = require('run-sequence').use(gulp);
-
-module.exports = function(config, log, error, success) {
-    gulp.task('default', function(callback) {
-        runSequence('clean', ['root', 'extension', 'html', 'images', 'scripts', 'styles'], success);
-        callback;
-    });
-};
